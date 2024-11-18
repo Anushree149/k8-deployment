e86d31c4deedd62090344bcbb9143a8ee03eaaf1
Anushree <idekaranushree@gmail.com>
2024-11-18 17:22:47 +0530

    docker build

--- script.groovy
+++ script.groovy
@@ -7,5 +7,10 @@ pipeline {
                 git branch: "main", url:'https://github.com/Ab-D-ev/kubernetes-devops-project.git' 
             } 
         } 
+    stage("docker build"){
+        sh 'docker image build -t $JOB_NAME:v1.$BUILD_ID .'
+        sh 'docker image tag $JOB_NAME:v1.$BUILD_ID anushree039/$JOB_NAME:v1.$BUILD_ID'
+        sh 'docker image tag $JOB_NAME:v1.$BUILD_ID anushree039/$JOB_NAME:latest'
+        }
     }
 }
