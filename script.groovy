8c9793a587c195981ae095f5e39a2e7ded21b851
Anushree <idekaranushree@gmail.com>
2024-12-02 12:13:34 +0530

    commit

--- script.groovy
+++ script.groovy
@@ -7,40 +7,5 @@ pipeline {
                 git branch: "main", url: 'https://github.com/Ab-D-ev/kubernetes-devops-project.git' 
             } 
         } 
-        
-        stage('Docker Build') {
-            steps {
-                script {
-                    sh 'docker image build -t $JOB_NAME:v1.$BUILD_ID .'
-                    sh 'docker image tag $JOB_NAME:v1.$BUILD_ID anushree039/$JOB_NAME:v1.$BUILD_ID'
-                    sh 'docker image tag $JOB_NAME:v1.$BUILD_ID anushree039/$JOB_NAME:latest'
-                }
-            }
-        }
-
-        stage('Docker Push') {
-            steps {
-                script {
-                    withCredentials([string(credentialsId: 'DockerPass', variable: 'DockerPass')]) {
-                        sh 'docker login -u anushree039 -p ${DockerPass}'
-                        sh 'docker image push anushree039/$JOB_NAME:v1.$BUILD_ID'
-                        sh 'docker image push anushree039/$JOB_NAME:latest'
-                    }
-                }
-            }
-        }
-
-        stage("Copy") {
-            steps {
-                script {
-                    sh "scp /var/lib/jenkins/workspace/k8-deployment/ansible-playbook.yml ubuntu@13.233.215.80:/tmp"
-                    sshagent(['ansible']) {
-                        sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.233.215.80'
-                        sh "mv /tmp/ansible-playbook.yml /home/ubuntu"
-                    } 
-                    
-                }
-            } 
-        }
     }
 }
