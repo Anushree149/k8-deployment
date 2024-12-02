bcae67238317b8854e6196107de0cb498743ac22
Anushree <idekaranushree@gmail.com>
2024-12-02 16:58:05 +0530

    Docker push

--- script.groovy
+++ script.groovy
@@ -1,27 +1,32 @@
-pipeline { 
-    agent any 
+pipeline {
+    agent any
 
-    stages { 
-        stage('Git Clone') { 
+    stages {
+        stage('Git Clone') {
             steps {
                 script {
-                    sshagent(['ansible']) {
-                        git branch: "main", url: 'https://github.com/Ab-D-ev/kubernetes-devops-project.git'
+                    sshagent(['ubuntu']) {
+                        sh '''
+                        ssh -o StrictHostKeyChecking=no ubuntu@3.6.36.148 << EOF
+                        cd /home/ubuntu/k8-deployment
+                        git clone https://github.com/Anushree149/k8-deployment.git || (cd k8-deployment && git pull)
+                        EOF
+                        '''
                     }
                 }
-            } 
-        } 
-        
+            }
+        }
+
         stage('Docker Build') {
             steps {
                 script {
-                    sshagent(['ansible']) {
+                    sshagent(['ubuntu']) {
                         sh '''
                         ssh -o StrictHostKeyChecking=no ubuntu@3.6.36.148 << EOF
-                        cd /home/ubuntu
-                        docker image build -t $JOB_NAME:v1.$BUILD_ID .
-                        docker image tag $JOB_NAME:v1.$BUILD_ID anushree039/$JOB_NAME:v1.$BUILD_ID
-                        docker image tag $JOB_NAME:v1.$BUILD_ID anushree039/$JOB_NAME:latest
+                        cd /home/ubuntu/k8-deployment
+                        docker build -t k8-deployment:v1.$BUILD_ID .
+                        docker tag k8-deployment:v1.$BUILD_ID anushree039/k8-deployment:v1.$BUILD_ID
+                        docker tag k8-deployment:v1.$BUILD_ID anushree039/k8-deployment:latest
                         EOF
                         '''
                     }
@@ -32,13 +37,13 @@ pipeline {
         stage('Docker Push') {
             steps {
                 script {
-                    sshagent(['ansible']) {
+                    sshagent(['ubuntu']) {
                         withCredentials([string(credentialsId: 'DockerPass', variable: 'DockerPass')]) {
                             sh '''
                             ssh -o StrictHostKeyChecking=no ubuntu@3.6.36.148 << EOF
                             echo ${DockerPass} | docker login -u anushree039 --password-stdin
-                            docker image push anushree039/$JOB_NAME:v1.$BUILD_ID
-                            docker image push anushree039/$JOB_NAME:latest
+                            docker push anushree039/k8-deployment:v1.$BUILD_ID
+                            docker push anushree039/k8-deployment:latest
                             EOF
                             '''
                         }
