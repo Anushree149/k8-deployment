44770e26e070386e3e68004e4f4a3619d052b010
Anushree <idekaranushree@gmail.com>
2024-12-16 16:58:16 +0530

    repo

--- script.groovy
+++ script.groovy
@@ -2,8 +2,10 @@ pipeline {
     agent any
 
     environment {
-        SERVER_IP = '43.205.126.199'  // Server IP address
+        //SERVER_IP = '43.205.126.199'  // Server IP address
         DOCKER_USER = 'anushree039'  // DockerHub username
+        ANSIBLE_HOST_IP = '43.205.126.200'  // Ansible Server IP
+        K8S_HOST_IP = '3.108.249.244'  // Kubernetes Server IP
     }
 
     stages {
@@ -12,7 +14,7 @@ pipeline {
                 script {
                     sshagent(['ansible']) {
                         sh """
-                        ssh -o StrictHostKeyChecking=no ubuntu@${SERVER_IP} 'cd /home/ubuntu/code && \
+                        ssh -o StrictHostKeyChecking=no ubuntu@${ANSIBLE_HOST_IP} 'cd /home/ubuntu/code && \
                         git clone -b main https://github.com/Anushree149/K8-Final.git'
                         """
                     }
@@ -25,7 +27,7 @@ pipeline {
                 script {
                     sshagent(['ansible']) {
                         sh """
-                        ssh -o StrictHostKeyChecking=no ubuntu@${SERVER_IP} 'cd /home/ubuntu/k8-deployment/kubernetes-devops-project && \
+                        ssh -o StrictHostKeyChecking=no ubuntu@${ANSIBLE_HOST_IP} 'cd /home/ubuntu/k8-deployment/kubernetes-devops-project && \
                         docker build -t k8-deployment:v1.${BUILD_ID} . && \
                         docker tag k8-deployment:v1.${BUILD_ID} ${DOCKER_USER}/k8-deployment:v1.${BUILD_ID} && \
                         docker tag k8-deployment:v1.${BUILD_ID} ${DOCKER_USER}/k8-deployment:latest'
@@ -41,13 +43,36 @@ pipeline {
                     sshagent(['ansible']) {
                         withCredentials([string(credentialsId: 'DockerPass', variable: 'DockerPass')]) {
                             sh """
-                            ssh -o StrictHostKeyChecking=no ubuntu@${SERVER_IP} 'echo ${DockerPass} | docker login -u ${DOCKER_USER} --password-stdin && \
+                            ssh -o StrictHostKeyChecking=no ubuntu@${ANSIBLE_HOST_IP} 'echo ${DockerPass} | docker login -u ${DOCKER_USER} --password-stdin && \
                             docker push ${DOCKER_USER}/k8-deployment:v1.${BUILD_ID} && \
                             docker push ${DOCKER_USER}/k8-deployment:latest && \
                             docker logout'
                             """
                         }
                     }
+                }
+            }
+        }
+
+        stage('Send Files to Ansible & K8 Servers') {
+            steps {
+                sshagent(['ansible']) {
+                sh """
+                scp -o StrictHostKeyChecking=no /home/ubuntu/code/K8-Final/service.yml ubuntu@${K8S_HOST_IP}:/home/ubuntu/
+                scp -o StrictHostKeyChecking=no /home/ubuntu/code/K8-Final/deployment.yml ubuntu@${K8S_HOST_IP}:/home/ubuntu/
+                """
+                }
+            }
+        }
+
+        stage('Run Ansible Playbook') {
+            steps {
+                sshagent(['ansible']) {
+                    sh """
+                    ssh -o StrictHostKeyChecking=no ubuntu@${ANSIBLE_HOST_IP} '
+                    ansible-playbook -i /home/ubuntu/my_inv /home/ubuntu/code/K8-Final/ansible-playbook.yml
+                    '
+                    """
                 }
             }
         }
