75391317a7998fa73e50b186bc81a5cca3f4b224
Anushree <idekaranushree@gmail.com>
2024-12-16 17:22:08 +0530

    repo

--- script.groovy
+++ script.groovy
@@ -27,7 +27,7 @@ pipeline {
                 script {
                     sshagent(['ansible']) {
                         sh """
-                        ssh -o StrictHostKeyChecking=no ubuntu@${ANSIBLE_HOST_IP} 'cd /home/ubuntu/k8-deployment/kubernetes-devops-project && \
+                        ssh -o StrictHostKeyChecking=no ubuntu@${ANSIBLE_HOST_IP} 'cd /home/ubuntu/code/K8-Final && \
                         docker build -t k8-deployment:v1.${BUILD_ID} . && \
                         docker tag k8-deployment:v1.${BUILD_ID} ${DOCKER_USER}/k8-deployment:v1.${BUILD_ID} && \
                         docker tag k8-deployment:v1.${BUILD_ID} ${DOCKER_USER}/k8-deployment:latest'
